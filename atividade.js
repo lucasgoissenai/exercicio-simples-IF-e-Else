3ccddc48f9807d866b1a5d082a5ad1a734b7529c
lucasgoissenai <lucas.gois@docente.fieto.br>
2024-01-25 15:43:41 -0300

    Add:resumo dos conteudos

---
+++ atividade.js
@@ -0,0 +1,9 @@
+let numero = parseFloat(prompt("Digite um número:"));
+
+if (numero > 0) {
+    console.log("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
